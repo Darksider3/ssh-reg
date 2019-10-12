b368ea058c5d231afedc4b57c51e7cb9955ce2d4
Darksider3 <github@darksider3.de>
2019-10-12 13:55:22 +0200

    huh, that already looks close to good... o.o

--- private/ListUsers.py
+++ private/ListUsers.py
@@ -15,13 +15,37 @@ class ListUsers:
         else:
             query = "SELECT * FROM `applications` WHERE status = '0'"
         self.usersFetch = self.db.query(query)
-        print(self.usersFetch)
+
+    def getFetch(self):
+        return self.usersFetch
 
 
 if __name__ == "__main__":
     try:
         L = ListUsers(CFG.args.unapproved)
-        print("hi")
+        fetch = L.getFetch()
+        # MAYBE best solution: https://pypi.org/project/texttable/
+        # examle:
+        """
+from texttable import Texttable
+t = Texttable()
+t.add_rows([['Name', 'Age'], ['Alice', 24], ['Bob', 19]])
+print(t.draw())
+---------------> Results in:
+
++-------+-----+
+| Name  | Age |
++=======+=====+
+| Alice | 24  |
++-------+-----+
+| Bob   | 19  |
++-------+-----+
+
+        """
+        for user in fetch:
+            print("ID: {}; Username: \"{}\"; Mail: <{}>; Name: \"{}\"; Registered: {}; Status: {}".format(
+                user["id"], user["username"], user["email"], user["name"], user["timestamp"], user["status"]
+            ))
         exit(0)
     except KeyboardInterrupt:
         pass
