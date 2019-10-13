4134e3cc2e34acf0c2b9f59744b5958acd20ede6
Darksider3 <github@darksider3.de>
2019-10-13 10:42:15 +0200

    introduces -f/--file, which outputs or takes from a file(conditional to the current program)

--- private/lib/CFG.py
+++ private/lib/CFG.py
@@ -15,6 +15,8 @@ argparser.add_argument('-u', '--unapproved', default=False, action="store_true",
                        help='only unapproved users. Default is only approved.', required=False)
 argparser.add_argument('-a', '--approved', default=False, action="store_true",
                        help="Only approved Users.", required=False)
+argparser.add_argument('-f', '--file', default="stdout",
+                       type=str, help='write to file instead of stdout', required=False)
 args = argparser.parse_args()
 
 CONF_FILE = args.config
