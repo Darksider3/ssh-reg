8c3ac4060f90b3c8a2a0b2fe6f9c4bff56a41279
Darksider3 <github@darksider3.de>
2019-10-19 14:02:52 +0200

    Split CFG.py to private/lib/cfgparse.py

--- private/lib/CFG.py
+++ private/lib/CFG.py
@@ -1,12 +1,6 @@
-import configparser
-import logging
-import lib.uis.default as default_cmd
+import lib.cfgparse
 
-args = default_cmd.argparser.parse_args()
-CONF_FILE = args.config
-config = configparser.ConfigParser()
-config.read(CONF_FILE)
-logging.basicConfig(format="%(asctime)s: %(message)s",
-                    level=int(config['LOG_LEVEL']['log_level'])
-                    )
-REG_FILE = config['DEFAULT']['applications_db']
+args = lib.cfgparse.args
+CONF_FILE = lib.cfgparse.CONF_FILE
+config = lib.cfgparse.config
+REG_FILE = lib.cfgparse.REG_FILE
