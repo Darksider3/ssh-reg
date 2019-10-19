670aa3d9c392e8e3da32fef5053744e040d07c7a
Darksider3 <github@darksider3.de>
2019-10-19 14:27:16 +0200

    Factor out CONF_FILE

--- private/lib/CFG.py
+++ private/lib/CFG.py
@@ -1,11 +1,6 @@
 import configparser
-import logging
 import lib.uis.default as default_cmd
 
 args = default_cmd.argparser.parse_args()
-CONF_FILE = args.config
 config = configparser.ConfigParser()
-config.read(CONF_FILE)
-logging.basicConfig(format="%(asctime)s: %(message)s",
-                    level=int(config['LOG_LEVEL']['log_level'])
-                    )
+config.read(args.config)
